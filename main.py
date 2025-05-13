a31b629f874d94bca1c2a601b08579524aacfdfd
Hakan Eser <31434581+eddiemummy@users.noreply.github.com>
2025-05-13 21:41:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ api_key = st.secrets["GOOGLE_GEMINI_KEY"]
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash", 
     google_api_key=api_key,
-    temperature=0.3 
+    temperature=0.4 
 )
 
 
